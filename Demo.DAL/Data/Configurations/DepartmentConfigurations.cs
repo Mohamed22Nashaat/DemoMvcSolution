58cfc29448d18933d7399e647b48fbb75d67290a
1 <Mohamed.Nashaat@code81.com>
2025-03-25 12:05:39 +0200

    Department Module {Entites & Configurations] and Dbcontext

---
+++ Demo.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Demo.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D=> D.Id).UseIdentityColumn(10,10);
+            builder.Property(D => D.Name).HasColumnType("varchar(20)");
+            builder.Property(D => D.Code).HasColumnType("varchar(10)");
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GETDATE()"); // For one time only
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GETDATE()"); // recalculation automatic
+        }
+    }
+}
---
+++ Demo.DAL/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Reflection;
+
+namespace Demo.DAL.Data.Contexts
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           // optionsBuilder.UseSqlServer("Server=.;Database=DemoDb;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<Department> Departments { get; set; } 
+    }
+}
