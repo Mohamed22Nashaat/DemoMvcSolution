2e7c978edda00ecee139bfa5d88a389c0e6120d1
1 <Mohamed.Nashaat@code81.com>
2025-03-26 13:38:50 +0200

    Departments Controller

---
+++ Demo.Presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Demo.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class DepartmentsController(IDepartmentServices _departmentServices) : Controller
+    {
+
+        // GET Base URL/Departments/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Departments = _departmentServices.GetAllDepartments();
+            return View(Departments);
+        }
+    }
+}
